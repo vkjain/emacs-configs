31118a3211e69bc4bf7eecd3a3335945bcab74b9
vkjain <vkj1428@gmail.com>
2019-09-13 16:07:36 +0530

    testing

--- init.el
+++ init.el
@@ -58,12 +58,7 @@
   (interactive)
   (load-file "~/.emacs.d/init.el"))
 
-;;--------------------------------------------------
-;; set keys for Apple keyboard, for emacs in OS X
-;; (setq mac-command-modifier 'meta) ; make cmd key do Meta
-;; (setq mac-option-modifier 'super) ; make opt key do Super
-;; (setq mac-control-modifier 'control) ; make Control key do Control
-;; (setq ns-function-modifier 'hyper)  ; make Fn key do Hyper
+
 
 
 ;; check OS type
